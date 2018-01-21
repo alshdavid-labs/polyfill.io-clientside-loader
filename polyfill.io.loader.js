ba7f670c0f4f8e96e1a57ed5ffd9c156c83d216e
David Alsh <alshdavid@users.noreply.github.com>
2018-01-21 17:35:44 +1100

    Update polyfill.io.loader.js

--- polyfill.io.loader.js
+++ polyfill.io.loader.js
@@ -1,5 +1,18 @@
+/*
+    Async polyfill loader
+    Will check your browser for the selected features and load them if you need them
+    This is built on top of polyfill.io
+    
+    Feel free to contribute, I have been adding polyfills to the list as I need them
+
+    David Alsh
+
+    https://github.com/alshdavid/polyfill.io-clientside-loader
+    PRs welcome
+*/
+
 var polyfills = {
-    load: function (polyfillsArr, callback, error) {
+    load: function(polyfillsArr, callback, error) {
         if (polyfillsArr.length == 0) {
             return callback()
         }
@@ -18,15 +31,15 @@ var polyfills = {
         if (!toAdd) return callback()
         polyfills.loadPolyfills(toAdd, callback, error)
     },
-    loadPolyfills: function (polyStr, callback, error) {
+    loadPolyfills: function(polyStr, callback, error) {
         var xhr = new XMLHttpRequest()
         xhr.open(
             "GET",
             "https://cdn.polyfill.io/v2/polyfill.min.js?features=" +
-            polyStr +
-            "&flags=gated,always&ua=chrome/50&callback=main"
+                polyStr +
+                "&flags=gated,always&ua=chrome/50&callback=main"
         )
-        xhr.onload = function () {
+        xhr.onload = function() {
             if (this.status >= 200 && this.status < 300) {
                 eval(xhr.response)
                 callback()
@@ -34,7 +47,7 @@ var polyfills = {
                 error(xhr.response)
             }
         }
-        xhr.onerror = function () {
+        xhr.onerror = function() {
             error({
                 status: this.status,
                 statusText: xhr.statusText
@@ -43,20 +56,28 @@ var polyfills = {
         xhr.send()
     },
     polyfills: {
-        "Object.assign": function () {
+        "Object.assign": function() {
             if ("assign" in Object) return true
         },
-        IntersectionObserver: function () {
+        "Array.prototype.find": function() {
+            if (Array.prototype.find) return true
+        },
+        "Array.prototype.includes": function() {
+            if (Array.prototype.includes) return true
+        },
+        IntersectionObserver: function() {
             if ("IntersectionObserver" in window) return true
         },
-        "Element.prototype.after": function () {
+        "Element.prototype.after": function() {
             if ("after" in Element.prototype) return true
         },
-        Promise: function () {
+        Promise: function() {
             if ("Promise" in window) return true
         },
-        fetch: function () {
+        fetch: function() {
             if ("fetch" in window) return true
         }
     }
 }
+
+module.exports = { polyfills }
